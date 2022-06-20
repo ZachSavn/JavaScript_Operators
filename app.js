c031716de80fffb18f413c627b35821f857ec908
zachary webb <zachsavn@gmail.com>
2022-06-20 16:31:01 -0400

    completed flex exercise

--- app.js
+++ app.js
@@ -5,15 +5,54 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
-
 // YOUR CODE HERE
 
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(a, b, add, minus, multiply, dividing)
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let a1 = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
 
+let q2a = a + str;
+let q2b = a + str2;
+let q2c = a + isPresent;
+let q2d = a + firstName;
+let q2e = a + lastName;
+
+console.log(q2a, q2b, q2c, q2d, q2e)
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+let q3a = d == str3;
+let q3b = d === str3;
+let q3c = !isPresent2;
+let q3d = ("eleven" == str4 && d >= str3);
+let q3e = (!isPresent2 || isPresent2);
+let q3f = 0 == false;
+let q3g = 0 ===false;
+let q3h = 0 !=false;
+let q3i = 0 !==false;
+
+console.log(q3a, q3b, q3c, q3d, q3e, q3f, q3g, q3h, q3i)
